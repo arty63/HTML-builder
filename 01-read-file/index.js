af051d3be7df071d67a9466fa2754fd74939e8aa
arty63 <artjom63@mail.ru>
2022-05-21 13:53:35 +0400

    fix: fixed read file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,14 @@
 const fs = require('fs')
 
-fs.readFile('./text.txt', function(err, data){
-    if(err){
-        console.error(err)
+const stream = new fs.ReadStream(__dirname + '/text.txt', {encoding: 'utf-8'})
+
+stream.on('data', function(chunk){
+    console.log(chunk)
+})
+stream.on('error', function(err){
+    if (err.code == 'ENOENT'){
+        console.log('Файл не найден')
     }else{
-        console.log(data.toString('utf-8'))
+        console.error(err)
     }
 })
